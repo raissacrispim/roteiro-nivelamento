7a5d68549bdf83ee68b13d152deba032efc66dd5
raissacrispim <raissa.crispim.700@ufrn.edu.br>
2026-04-02 17:18:50 -0300

    Criando o primeiro arquivo .tcl da Parte 7

---
+++ hello.tcl
@@ -0,0 +1,8 @@
+puts "Olá, Mundo - Entre aspas"     ;# Este é um comentário após o comando.
+# Este é um comentário no início de uma linha
+puts {Olá, Mundo - Entre chaves}
+puts {Exemplo de sintaxe de comentário inválida}  # *Erro* - não há ponto e vírgula!
+puts "Esta é a linha 1"; puts "esta é a linha 2"
+puts "Olá, Mundo; - Com um ponto e vírgula dentro das aspas"
+# Palavras não precisam ser colocadas entre aspas, a menos que contenham espaços
+puts OláMundo
